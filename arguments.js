ac1da23e7b58b754e30f4092c02e16321a76833c
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-09 14:01:07 -0700

    new activities

--- arguments.js
+++ arguments.js
@@ -2,4 +2,9 @@
 //[ '/usr/local/bin/node',
 //  '/Users/michaelkovacic/Desktop/Projects/NodeCode/arguments.js' ]
 
-console.log(process.argv);
+console.log(process.argv[2] + process.argv[3]);
+
+console.log(parseFloat(process.argv[2]) + parseFloat(process.argv[3]));
+
+console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
+
---
+++ parameter.js
@@ -0,0 +1,31 @@
+
+//parameter check
+//create a command line node application that takes in two parameters and outputs whether they are equal or not.
+
+//start by simply logging the value of each argument to console.  then use your usual Javascript approach
+
+
+
+// var value1 = parseFloat(process.argv[2]);
+
+// var value2 = parseFloat(process.argv[3]);
+
+
+// if(value1 === value2){
+// 	console.log(parseFloat(process.argv[2]) + "is equal to" + parseFloat(process.argv[3]));
+// }else{
+// 	console.log(parseFloat(process.argv[2]) + "is not equal to" + parseFloat(process.argv[3]));
+// }
+
+
+function test (value1, value2){
+	if(value1 === value2){
+	console.log("is equal to");
+	}else{
+	console.log("is not equal to");
+	}
+}
+
+test(parseFloat(process.argv[2]), parseFloat(process.argv[3]));
+
+console.log(parseFloat(process.argv[2]) === parseFloat(process.argv[3]));
