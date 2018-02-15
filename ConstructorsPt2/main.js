23e9af9ea6ef80371f13a860f896340107f3a91e
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-02-15 11:47:49 -0700

    new project

---
+++ ConstructorsPt2/main.js
@@ -0,0 +1,9 @@
+var Classroom = require("./classroom");
+
+
+var class1 = new Classroom("Mr. Mueller", "Room 201");
+
+class1.addStudent("William", "Math", "3.6");
+
+console.log(Classroom);
+console.log("---------------");
